6ec569b9e37c5e4df2d0f35de6496ac8ce6fe975
Kamal Sahmoud <maroc.develop@gmail.com>
2021-11-03 00:55:39 +0100

    .

--- src/admin/dto/color.dto.ts
+++ src/admin/dto/color.dto.ts
@@ -1,7 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsEmail } from 'class-validator';
 
 @InputType()
 export class CreateColorInput {
   @Field(() => String)
+  @IsNotEmpty({ message: "First name can't be blank." })
+  @IsEmail({}, { message: 'Invalid email format' })
   title: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Class validator.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
