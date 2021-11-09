9ff7edef3680fe12195dd797d8c31a03e2a41cf3
Kamal Sahmoud <maroc.develop@gmail.com>
2021-11-09 17:13:36 +0100

    upload file to cloudinary

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { graphqlUploadExpress } from 'graphql-upload';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable upload files.
+  app.use(graphqlUploadExpress());
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
--- src/resolvers/resolvers.module.ts
+++ src/resolvers/resolvers.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 // Modules.
 import { AdminModule } from '@src/admin/admin.module';
 import { DatabaseModule } from '@src/database/database.module';
+import { MediaModule } from '@src/media/media.module';
 // Resolvers.
 import { CatResolver } from './admin/cat/cat.resolver';
 import { ColorResolver } from './admin/color/color.resolver';
+import { CloudinaryResolver } from './cloudinary/cloudinary.resolver';
 
 @Module({
-  imports: [DatabaseModule, AdminModule],
-  providers: [CatResolver, ColorResolver],
+  imports: [DatabaseModule, AdminModule, MediaModule],
+  providers: [CatResolver, ColorResolver, CloudinaryResolver],
 })
 export class ResolversModule {}
